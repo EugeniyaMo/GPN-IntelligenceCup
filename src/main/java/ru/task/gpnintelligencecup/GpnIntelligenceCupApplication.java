1319fc616445dbfe3fbe0df4ecc064bf7410ee0f
eugeniya.mo <eugeniya.mo@gmail.com>
2022-11-02 15:47:50 +0300

    Initial commit

---
+++ src/main/java/ru/task/gpnintelligencecup/GpnIntelligenceCupApplication.java
@@ -0,0 +1,13 @@
+package ru.task.gpnintelligencecup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpnIntelligenceCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpnIntelligenceCupApplication.class, args);
+	}
+
+}
